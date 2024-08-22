c7d35f50c87b66790020f716748242f1e16341db
probrahim <ibrahimhassani674@gmail.com>
2024-08-22 15:35:55 +0100

    v1 && v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TACHEComponent } from './tache/tache.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import { InventaireComponent } from './inventaire/inventaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TACHEComponent,
+    InventaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
